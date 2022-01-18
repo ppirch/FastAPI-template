c41a685b3a5f5db2bf7a706df4fbccfdd0e640fa
Pakin Pirch <pakin.pirch@gmail.com>
2022-01-18 16:50:54 +0000

    Add main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routes.student import router as StudentRouter
 from routes.admin import router as AdminRouter
 
 from fastapi_pagination import add_pagination
+
 
 app = FastAPI()
 
@@ -17,3 +19,7 @@ app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
 app.include_router(StudentRouter, tags=["Students"], prefix="/student")
 
 add_pagination(app)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8083)
